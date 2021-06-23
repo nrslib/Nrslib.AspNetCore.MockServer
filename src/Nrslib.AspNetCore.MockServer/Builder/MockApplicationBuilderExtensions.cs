13f0083f3f52213f5dfb94f4d855a6b56b1e5c55
nrslib <masanobu.naruse@gmail.com>
2021-06-23 23:52:40 +0900

    first commit

---
+++ src/Nrslib.AspNetCore.MockServer/Builder/MockApplicationBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using nrslib.AspNetCore.MockServer.Middlewares;
+
+namespace nrslib.AspNetCore.MockServer.Builder
+{
+    public static class MockApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseMock(this IApplicationBuilder app)
+        {
+            if (app == null) throw new ArgumentNullException(nameof(app));
+
+            return app.UseMiddleware<MockMiddleware>();
+        }
+    }
+}
---
+++ src/Nrslib.AspNetCore.MockServer/Middlewares/MockMiddleware.cs
@@ -0,0 +1,81 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using File = System.IO.File;
+
+
+namespace nrslib.AspNetCore.MockServer.Middlewares
+{
+    public class MockMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly IConfiguration configuration;
+
+        public MockMiddleware(RequestDelegate next, IConfiguration configuration)
+        {
+            this.next = next;
+            this.configuration = configuration;
+        }
+
+        public async Task Invoke(HttpContext context, IHostingEnvironment env)
+        {
+            var controllerName = (string) context.GetRouteValue("controller");
+            var actionName = (string) context.GetRouteValue("action");
+
+            if (controllerName is null || actionName is null)
+            {
+                await next(context);
+                return;
+            }
+
+            var (targetFileExists, filePath) = GetTargetFilePath(env, controllerName, actionName);
+
+            if (!targetFileExists)
+            {
+                await next(context);
+                return;
+            }
+
+            var content = await File.ReadAllTextAsync(filePath);
+            if (ShouldIgnore(content))
+            {
+                await next(context);
+                return;
+            }
+
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(content);
+        }
+
+        private (bool, string) GetTargetFilePath(IHostingEnvironment env,string controllerName, string actionName)
+        {
+            var relationalPath = configuration["MockServerConfig:MockRootPath"] ?? "Mock";
+            var filePath = Path.Combine(env.ContentRootPath, relationalPath, controllerName, actionName + ".json");
+
+            return File.Exists(filePath) 
+                ? (true, filePath) 
+                : (false, null);
+        }
+
+        private bool ShouldIgnore(string jsonText)
+        {
+            if (string.IsNullOrWhiteSpace(jsonText))
+            {
+                if (!bool.TryParse(configuration["MockServerConfig:IgnoreBlank"], out var ignoreBlank))
+                {
+                    return true;
+                }
+
+                if (ignoreBlank)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
